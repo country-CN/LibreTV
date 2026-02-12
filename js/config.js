a4193261f21eafd90ae63fb539b9d5f106dbcb5c
country-CN <597020759@qq.com>
2026-02-13 04:17:34 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,29 +22,43 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
-    testSource: {
-        api: 'https://www.fantuan.tv/api.php/provide/vod',
-        name: '饭团影视',
-        api: 'https://cj.lziapi.com/api.php/provide/vod',
-        name: '影视工厂',
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
-        api: 'http://www.ryzyw.com/api.php/provide/vod',
+         api: 'http://www.ryzyw.com/api.php/provide/vod',
         name: '如意资源',
-        api: 'https://www.moduzy.net/api.php/provide/vod',
-        name: '魔都资源',
-        api: 'http://tsg.zmdnx.com/api.php/provide/vod',
-        name: '非凡影视',
-        api: 'http://ffzy5.tv/api.php/provide/vod',
-        name: '非凡资源',
-        api: 'https://www.tztqtyn.com/api.php/provide/vod',
-        name: '天涯资源',
-        api: 'https://www.cqbfw.com/pianku/4/',
-        name: '暴风资源',
-        adult: true
-    }
-    //ARCHIVE https://telegra.ph/APIs-08-12
-};
+    },
+ +  wwzy:{ 
+ +       api: 'https://www.fantuan.tv/api.php/provide/vod',
+ +       name: '饭团影视',
+ +  },
+ +  wwzy:{  
+ +       api: 'https://cj.lziapi.com/api.php/provide/vod',
+ +       name: '影视工厂',
+ +  },
+ +  ikun:{
+ +       api: 'https://www.qiqidys.com/api.php/provide/vod',
+ +       name: '七七资源',
+ +  },  
+ +  ikun:{ 
+ +       api: 'https://www.moduzy.net/api.php/provide/vod',
+ +        name: '魔都资源',
+ +  },
+ +  ikun:{
+ +       api: 'http://tsg.zmdnx.com/api.php/provide/vod',
+ +       name: '非凡影视',
+ +  },   
+ +  ikun:{
+ +       api: 'http://ffzy5.tv/api.php/provide/vod',
+ +       name: '非凡资源',
+ +  }, 
+ +  ikun:{
+ +       api: 'https://www.tztqtyn.com/api.php/provide/vod',
+ +       name: '天涯资源',
+ +  ikun:{
+ +       api: 'https://www.cqbfw.com/pianku/4/',
+ +       name: '暴风资源',
+    },
+   };
+//添加聚合搜索的配置选项
+
 
 // 定义合并方法
 function extendAPISites(newSites) {
